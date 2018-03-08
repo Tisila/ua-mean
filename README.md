51fc6808d758d349af701abe981ded7807d13624
Tiago Avila <tiagoavila44@gmail.com>
2018-03-08 23:22:13 +0000

    Importing script along with a simple readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ua-mean
 Compute University of Aveiro student marks mean
+
+### Quick summary
+This is a simple script to compute the user's marks average.
+
+###  Version
+0.1
+
+### Summary of set up
+The easiest way is to access this script through [Greasy Fork](https://greasyfork.org) on this [link](https://greasyfork.org).
+
+### Repo owner
+That would be me, Tisila.
---
+++ UA-Mean.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         UA Mean
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Compute marks mean
+// @author       Tiago Avila
+// @match        https://paco.ua.pt/secvirtual/c_historiconotas.asp
+// @grant        none
+// ==/UserScript==
+
+var marks = [];
+var marksTable = document.getElementById("historico");
+var marksRows = marksTable.rows;
+var lastIndex = marksRows.length - 1;
+
+for (var row in marksRows) {
+    if (row >= 1 && row < lastIndex) {
+        var mark = marksRows[row].cells[3].innerHTML;
+        //console.log(mark);
+        marks.push(parseInt(mark));
+    }
+}
+var mean = mean(marks);
+refresh(mean);
+
+function mean(array) {
+    var total = 0;
+    for (var i in array) {
+        total += array[i];
+    }
+    var totalMean = total / array.length;
+    return round(totalMean,3);
+}
+
+function refresh(mean) {
+    var lastRow = marksRows[lastIndex].cells[0];
+    var desc = lastRow.innerHTML;
+    lastRow.innerHTML = desc + "  |  Média: " + mean;
+}
+
+function round(value, decimals) {
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
